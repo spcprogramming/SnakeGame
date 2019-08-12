682dac504a82e5acea712bfacefa67ce0d7d1766
sp.student <sp.student@Acad.SPCollege.edu>
2019-08-12 18:55:57 -0400

    Add project files.

---
+++ SnakeCSharp/Snake.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace SnakeCSharp
+{
+    partial class Snake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainGraphic = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.mainGraphic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainGraphic
+            // 
+            this.mainGraphic.Location = new System.Drawing.Point(12, 12);
+            this.mainGraphic.Name = "mainGraphic";
+            this.mainGraphic.Size = new System.Drawing.Size(848, 625);
+            this.mainGraphic.TabIndex = 0;
+            this.mainGraphic.TabStop = false;
+            this.mainGraphic.Paint += new System.Windows.Forms.PaintEventHandler(this.mainGraphic_Paint);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 1000;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 649);
+            this.Controls.Add(this.mainGraphic);
+            this.Name = "Snake";
+            this.Text = "Snake";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.mainGraphic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainGraphic;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ SnakeCSharp/Snake.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeCSharp
+{
+    public partial class Snake : Form
+    {
+        Bitmap b;
+        int xPos;
+        int yPos;
+        int increment;
+        Direction d;
+        Point previous;
+
+        public Snake()
+        {
+            InitializeComponent();
+            xPos = mainGraphic.Width / 2;
+            yPos = mainGraphic.Height / 2;
+            d = Direction.Left;
+            previous = new Point(mainGraphic.Width / 2, mainGraphic.Height / 2);
+            increment = 10;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            switch (d)
+            {
+                case Direction.Down:
+                    yPos += increment;
+                    break;
+                case Direction.Up:
+                    yPos -= increment;
+                    break;
+                case Direction.Left:
+                    xPos -= increment;
+                    break;
+                case Direction.Right:
+                    xPos += increment;
+                    break;
+            }
+
+            if(b != null)
+            {
+                b.Dispose();
+                b = null;
+            }
+               
+            b = new Bitmap(mainGraphic.Width, mainGraphic.Height);
+            Graphics g = Graphics.FromImage(b);
+
+            Pen p = new Pen(Color.Plum, 5);
+            Point current = new Point(xPos, yPos);
+
+            g.DrawLine(p, previous, current);
+            previous = current;
+
+            g.Dispose();
+            mainGraphic.Invalidate();
+        }
+
+        private void mainGraphic_Paint(object sender, PaintEventArgs e)
+        {
+
+            if (b != null)
+            {
+                Graphics g = e.Graphics;
+                g.DrawImage(b, 0, 0, b.Width, b.Height);
+            }
+
+        }
+
+        private void Snake_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    d = Direction.Left;
+                    break;
+                case Keys.Right:
+                    d = Direction.Right;
+                    break;
+                case Keys.Down:
+                    d = Direction.Down;
+                    break;
+                case Keys.Up:
+                    d = Direction.Up;
+                    break;
+            }
+        }
+
+        private enum Direction
+        {
+            Left,
+            Right,
+            Up,
+            Down
+        }
+    }
+}
